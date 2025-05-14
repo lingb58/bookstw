c0a45f2320f49862529d2c959df89818c27ac6ba
Robin Lin <lingb58@hotmail.com>
2025-05-15 00:27:52 +0800

    fix: touched_fields should consist with each query. parsing comment included too much info.

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ class BooksTW(Source):
     capabilities = frozenset(['identify'])#, 'cover'])
     touched_fields = frozenset([
         'identifier:isbn', 'identifier:bookstw', 'title', 'authors','rating', 
-        'comments', 'publisher', 'pubdate', 'languages'])  #'tags', 'series',
+        'comments', 'publisher', 'pubdate', 'tags', 'languages'])  # 'series',
 
     BASE_URL = 'https://www.books.com.tw'
 
@@ -104,6 +104,7 @@ class BooksTW(Source):
         else:
             self._query_count += 1
 
+        touched = []
         try:
             r = self.browser.open(book_url, timeout=timeout)
             raw = r.read()
@@ -128,6 +129,7 @@ class BooksTW(Source):
         if sub_title:
             title += ' ' + sub_title
         log.info("title: " + title)
+        touched.append('title')
 
         book_info_block = doc.xpath('.//div[@class="type02_p003 clearfix"]/ul/li')
         log.info(f"doc.book_info_block: {book_info_block}")
@@ -147,6 +149,7 @@ class BooksTW(Source):
                             if len(author) > 0:
                                 log.info("作者: " + author)
                                 authors.append(author)
+                touched.append('authors')
                 continue
             if info.text and "出版社：" in info.text:
                 publisher = "".join(info.itertext())
@@ -155,6 +158,7 @@ class BooksTW(Source):
                     end = publisher.index("\n")
                 publisher = publisher[publisher.index("：")+1:end]
                 log.info("出版社: " + publisher)
+                touched.append('publisher')
                 continue
             if info.text and "出版日期：" in info.text:
                 pubdate = info.text.replace("出版日期：", "")
@@ -164,41 +168,67 @@ class BooksTW(Source):
                 lang = info.text.replace("語言：", "").strip()#.replace("英文", "英語")
                 lang = lang_map[lang]
                 log.info("lang: " + lang)
-
-        content = ''.join(doc.xpath('//div[@class="mod_b type02_m057 clearfix"]/div//text()'))
-        isbn = ''.join(doc.xpath('//div[@class="mod_b type02_m058 clearfix"]/div/ul[1]/li[1]/text()')).removeprefix("ISBN：")
-        log.info("isbn: " + isbn)
-        if isbn:
-            self.cache_identifier_to_cover_url(isbn, cover_url)
+                touched.append('language')
+                continue
 
         meta = Metadata(title, authors)
+        contents = doc.xpath('//div[@class="mod_b type02_m057 clearfix"]')
+        for a in contents:
+            itertext = ''.join(a.itertext())
+            if '內容簡介' in itertext:
+                comments = itertext.replace('內容簡介', "")
+                while "\n\n" in comments:
+                    comments = comments.replace("\n\n", "\n")
+        meta.comments = comments
+        touched.append('comments')
+
+        isbn = ''.join(doc.xpath('//div[@class="mod_b type02_m058 clearfix"]/div/ul[1]/li[1]/text()')).removeprefix("ISBN：")
+        if isbn:
+            self.cache_identifier_to_cover_url(isbn, cover_url)
         meta.identifiers = {'isbn': isbn, 'bookstw': book_id}
-        meta.comments = content
+        touched.append('identifier:isbn')
+        touched.append('identifier:bookstw')
+
         meta.publisher = publisher
         log.info("publisher: " + meta.publisher)
+
         if pubdate:
             from calibre.utils.date import parse_date, utcnow
             try:
                 default = utcnow().replace(day=15)
                 meta.pubdate = parse_date(pubdate, assume_utc=True, default=default)
+                touched.append('pubdate')
             except:
                 log.error('Failed to parse pubdate %r' % pubdate)
+
         language = canonicalize_lang(lang)
         if language:
             meta.language = language
         else:
             meta.language = None
-        
         log.info("language: " + meta.language)
+        touched.append('language')
+
         rate = ''.join(doc.xpath('.//div[@class="bui-stars star-s"]/span/@title')).strip()
         log.info(f"rate: {rate}")
         if rate:
             meta.rating = float(rate[0:1])
-        else:
-            meta.rating = float(0)
-        log.info(f"rating: {meta.rating}")
+            if meta.rating > 0.0:
+                touched.append('rating')
+                log.info(f"rating: {meta.rating}")
+
+        tags = []
+        tag_text = ''.join(doc.xpath('.//ul[@class="sort"]//text()')).strip()
+        log.info(f"tag_text: {tag_text}")
+        tag_text = tag_text.replace("本書分類：", "").replace(" ", "")
+        tags = tag_text.split(">")
+        log.info(f"tags: {tags}")
+        meta.tags = tags
+        touched.append('tags')
+
         meta.has_cover = False
         meta.source_relevance = 0
+        self.touched_fields = frozenset(touched)
         return meta
 
     def download_cover(self, log, result_queue, abort,  # {{{
@@ -250,9 +280,13 @@ if __name__ == '__main__':  # tests
     test_identify_plugin(BooksTW.name,
         [
             (# A book with an ISBN
-                {'identifiers': {'isbn': '9789866272547'}},
-                [ title_test('攝影師的四大修練：打破規則的觀察、想像、表現、視覺設計，拍出大師級作品 Photography and the Art of Seeing: A Visual Perception Workshop for Film and Digital Photography', exact=True), authors_test(['patterson', 'freeman', '佛利曼．帕德遜']) ]
+                {'identifiers': {'isbn': '9787302527459'}},
+                [ title_test('ROS2源代碼分析與工程應用', exact=True), authors_test(['丁亮']) ]
             ),
+            # (# A book with an ISBN
+            #     {'identifiers': {'isbn': '9789866272547'}},
+            #     [ title_test('攝影師的四大修練：打破規則的觀察、想像、表現、視覺設計，拍出大師級作品 Photography and the Art of Seeing: A Visual Perception Workshop for Film and Digital Photography', exact=True), authors_test(['patterson', 'freeman', '佛利曼．帕德遜']) ]
+            # ),
             # (# A book with an ISBN
             #     {'identifiers': {'isbn': '9787532770243'}},
             #     [ title_test('石黑一雄（Kazuo Ishiguro）文集:被掩埋的巨人', exact=True), authors_test(['（英）石黑一雄']) ]
@@ -262,24 +296,12 @@ if __name__ == '__main__':  # tests
             #     [ title_test('再啟動︰獲取職場生存與發展的原動力', exact=True), authors_test(['[日]大前研一']) ]
             # ),
             # (# A book with an ISBN
-            #     { 'identifiers': { 'isbn': '9781596591707' } ] },
-            #     [ title_test('The First Time Manager', exact=True), authors_test(['Belker, Loren B./ Topchik, Gary S./ Pratt, Sean (NRT)']) ]
+            #     { 'identifiers': { 'isbn': '9781596591707' } },
+            #     [ title_test('The First Time Manager', exact=True), authors_test(['topchik', 'belker', 'b./', 'pratt', 'sean', 'loren', '(nrt)', 's./', 'gary']) ]
             # ),
             # (# A book with an ISBN
             #     { 'identifiers':{'isbn': '9780593420584'} },
-            #     [ title_test('Ideaflow: The Only Business Metric That Matters', exact=True), authors_test(['Utley, Jeremy,Klebahn, Perry']) ]
-            # ),
-            # (# A book with an ISBN
-            #     {'identifiers':{'isbn': '9780439064866'} },
-            #     [ title_test('Harry Potter and the Chamber of Secrets 美版精裝  哈利波特(2) ：消失的密室', exact=True), authors_test(['Rowling, J. K./ GrandPre, Mary (ILT)']) ]
-            # ),
-            # (# A book with no ISBN specified
-            #     { 'authors':['陈建伟'] },
-            #     [ title_test("把话说到点子上", exact=True), authors_test(['陈建伟'])]
-            # ),
-            # (# A book with no ISBN specified
-            #     { 'identifiers': {'mobi-asin': 'c75ada8f-5b95-40a5-86ed-d73a5ea64b48'}, 'title':"把话说到点子上", 'authors':['陈建伟'] },
-            #     [ title_test("把话说到点子上", exact=True), authors_test(['陈建伟'])]
+            #     [ title_test('Ideaflow: The Only Business Metric That Matters', exact=True), authors_test(['klebahn', 'perry', 'jeremy', 'utley']) ]
             # ),
             # (# A book with no ISBN specified
             #     { 'title':"BCG問題解決力：一生受用的策略顧問思考法", 'authors':['徐瑞廷'] },
@@ -288,13 +310,5 @@ if __name__ == '__main__':  # tests
             # (# A book with no ISBN specified
             #     { 'title':"再啟動︰獲取職場生存與發展的原動力", 'authors':['[日]大前研一'] },
             #     [ title_test("再啟動︰獲取職場生存與發展的原動力", exact=True), authors_test(['[日]大前研一'])]
-            # ),
-            # (# A book with no ISBN specified
-            #     { 'title':"天選矽島", 'authors':['黃欽勇'] },
-            #     [ title_test("天選．矽島：川普風暴下的科技島", exact=True), authors_test(['黃欽勇']) ]
-            # ),
-            # (# A book with no ISBN specified
-            #     { 'title':"Harry Potter and the Sorcerer's Stone", 'authors':['J.K. Rowling'] },
-            #     [ title_test("Harry Potter and the Sorcerer's Stone", exact=True),  authors_test(['J. K. Rowling'])]
             # ),
         ])
